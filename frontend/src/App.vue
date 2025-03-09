521976b1225970fd627d1f8f708a1ff30a8b7e13
Yukiteno <yunomyworld@gmail.com>
2025-03-09 21:23:28 +0800

    initialize project structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Title from './components/Title.vue';
+</script>
+
+<template>
+  <main>
+    <Title title="Game News Center" />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/Title.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+}
+</style>
