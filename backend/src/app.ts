5dd04968928c3dfc9589b2523f140708f1bf18c1
Yukiteno <yunomyworld@gmail.com>
2025-03-09 22:27:20 +0800

    introduce nodemon on express

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
