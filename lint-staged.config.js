d4f332045597aeb10100e68ad425842bd736dcb7
Yukiteno <yunomyworld@gmail.com>
2025-03-21 00:13:35 +0800

    chore: remove tests for now

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 export default {
-  './backend/**/*.{js,ts}': 'pnpm --filter backend test',
   '*.{js,ts,vue}': ['eslint --fix', 'prettier --write'],
   '*.{json,md,html}': ['prettier --write'],
   '*.css': ['prettier --write', 'stylelint --fix'],
