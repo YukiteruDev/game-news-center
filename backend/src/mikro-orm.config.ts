a452bc69a5c8768d09007f70bc02af8e100407f5
Yukiteno <yunomyworld@gmail.com>
2025-03-28 23:28:02 +0800

    fix(docker): fix puppeteer installation

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -6,7 +6,6 @@ import { NewsModel } from './models/news.model.js';
 console.table({
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
   DB_HOST: process.env.DB_HOST,
   debug: process.env.NODE_ENV !== 'production',
 });
